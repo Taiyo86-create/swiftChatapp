4907da94400a901fe1162f269e84cdb6a29a39d4
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-17 00:41:24 +0900

    リファクタリング

--- swiftChatapp/chatView.swift
+++ swiftChatapp/chatView.swift
@@ -10,55 +10,68 @@ import SwiftUI
 struct chatView: View {
     var body: some View {
         VStack(spacing: 0){
-                ScrollView{
-                    VStack(spacing: 0){
-                        ForEach(0..<30){ _ in
-                        HStack{
-                            Circle()
-                                .frame(width: 60, height: 60)
-                            Capsule()
-                                .frame(height: 60)
-                        }
-                        .padding(.bottom)
-                    }
-            }
-                    .padding(.horizontal)
-                    .padding(.top, 72 )
-            
-            }.background(.cyan)
+               messageArea
                 .overlay(
-                    HStack{
-                        Circle()
-                            .frame(width: 40, height: 40)
-                        Text("Titile")
-                        Spacer()
-                        Circle()
-                            .frame(width: 40, height: 40)
-                        Circle()
-                            .frame(width: 40, height: 40)
-                    }.foregroundColor(.white)
-                        .padding()
-                        .background(.black.opacity(0.5))
-                        ,alignment: .top
+                    navigationArea
+                    ,alignment: .top
                 )
-            
-            HStack{
-                Circle()
-                    .frame(width: 40, height: 40)
-                Circle()
-                    .frame(width: 40, height: 40)
-                Circle()
-                    .frame(width: 40, height: 40)
-                Capsule()
-                    .frame(height: 40)
-                Circle()
-                    .frame(width: 40, height: 40)
-            }.background(.white)
-                .padding()
+            inputArea
         }
     }
 }
 
 #Preview {
     chatView()
+}
+
+extension chatView{
+    private var messageArea: some View{
+        ScrollView{
+            VStack(spacing: 0){
+                ForEach(0..<30){ _ in
+                    HStack{
+                        Circle()
+                            .frame(width: 60, height: 60)
+                        Capsule()
+                            .frame(height: 60)
+                    }
+                    .padding(.bottom)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 72 )
+            
+        }.background(.cyan)
+    }
+    
+    private var inputArea: some View{
+        HStack{
+            Circle()
+                .frame(width: 40, height: 40)
+            Circle()
+                .frame(width: 40, height: 40)
+            Circle()
+                .frame(width: 40, height: 40)
+            Capsule()
+                .frame(height: 40)
+            Circle()
+                .frame(width: 40, height: 40)
+        }.background(.white)
+            .padding()
+    }
+    
+    private var navigationArea: some View{
+        HStack{
+            Circle()
+                .frame(width: 40, height: 40)
+            Text("Titile")
+            Spacer()
+            Circle()
+                .frame(width: 40, height: 40)
+            Circle()
+                .frame(width: 40, height: 40)
+        }.foregroundColor(.white)
+            .padding()
+            .background(.black.opacity(0.5))
+    }
 }
