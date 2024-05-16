f6987ea38602f06760c76b9ed8e97551c6b981fc
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-16 23:25:07 +0900

    Initial Commit

---
+++ swiftChatapp/swiftChatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftChatappApp.swift
+//  swiftChatapp
+//
+//  Created by 水元太陽 on 2024/05/16.
+//
+
+import SwiftUI
+
+@main
+struct swiftChatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
