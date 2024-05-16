78105404d2eeade6f29c6d8ad7e93b4b7809c776
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-16 23:35:05 +0900

    chat画面のファイル作成

---
+++ swiftChatapp/chatView.swift
@@ -0,0 +1,18 @@
+//
+//  chatView.swift
+//  swiftChatapp
+//
+//  Created by 水元太陽 on 2024/05/16.
+//
+
+import SwiftUI
+
+struct chatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    chatView()
+}
