f2ffb3fa874d4479e0fb3572b178aa0c5d29050a
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-17 20:00:48 +0900

    チャット一覧画面の作成

---
+++ swiftChatapp/views/listView.swift
@@ -0,0 +1,18 @@
+//
+//  listView.swift
+//  swiftChatapp
+//
+//  Created by 水元太陽 on 2024/05/17.
+//
+
+import SwiftUI
+
+struct listView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listView()
+}
