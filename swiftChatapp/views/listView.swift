08ed81c7f4a86ffba83f3eae0074372357bb7ebc
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-17 20:18:27 +0900

    リスト画面のざっくりフロント

--- swiftChatapp/views/listView.swift
+++ swiftChatapp/views/listView.swift
@@ -9,8 +9,48 @@ import SwiftUI
 
 struct listView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Text("トーク")
+                    .font(.title2.bold())
+                Spacer()
+                
+                HStack{
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    
+                    Circle()
+                        .frame(width: 40, height: 40)
+                }
+            }
+            
+            ScrollView{
+                VStack{
+                    ForEach(0..<5){_ in
+                        HStack{
+                            Circle()
+                                .frame(width: 60, height: 60)
+                            VStack(alignment: .leading){
+                                Text("タイトル")
+                                Text("最新のメッセージ")
+                                    .font(.footnote)
+                                    .foregroundColor(Color(uiColor: .secondaryLabel))
+                            }
+                            Spacer()
+                            Text("12/31")
+                                .font(.caption)
+                                .foregroundColor(Color(uiColor: .secondaryLabel))
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.horizontal)
     }
+        
 }
 
 #Preview {
