1ae9197e865e8e322f85dad0c6e829af2100ac71
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-17 23:06:45 +0900

    投稿日時の表示

--- swiftChatapp/ViewModels/SwiftChatappModel.swift
+++ swiftChatapp/ViewModels/SwiftChatappModel.swift
@@ -43,11 +43,16 @@ class ChatViewModel: ObservableObject{
         guard let index = chatData.firstIndex(where: { chat in
             chat.id == chatId
         }) else { return }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let formattedDateString = formatter.string(from: Date())
+        
         let newMessage = Message(
             id: UUID().uuidString,
             text: text,
             user: User.currentUser,
-            date: Date().description,
+            date: formattedDateString,
             readed: false
         )
         
