5f94759ff08aa5878debe2356783d599d396b7a5
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-17 23:56:24 +0900

    アイコンの表示

--- swiftChatapp/views/listView.swift
+++ swiftChatapp/views/listView.swift
@@ -63,10 +63,21 @@ extension listView{
     
     private func listRow(chat: Chat) -> some View {
         HStack{
-            Image("megaRcario")
-                .resizable()
-                .frame(width: 48, height: 48)
-                .clipShape(Circle())
+            let images = vm.getImages(messages: chat.messages)
+            HStack(spacing: -26){
+                ForEach(images, id: \.self){ image in
+                    Image(image)
+                        .resizable()
+                        .frame(width: 48, height: 48)
+                        .clipShape(Circle())
+                        .background(
+                           Circle()
+                            .foregroundColor(Color(uiColor: .systemBackground))
+                            .frame(width: 54, height: 54)
+                        )
+                }
+            }
+
             VStack(alignment: .leading){
                 Text(vm.getTitle(messages: chat.messages))
                     .lineLimit(1)
