2ca7eb00691f51ae7da320837275316dee888aa8
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-17 13:51:25 +0900

    リファクタリング

--- swiftChatapp/MessageRow.swift
+++ swiftChatapp/MessageRow.swift
@@ -10,33 +10,50 @@ import SwiftUI
 struct MessageRow: View {
     var body: some View {
         HStack(alignment: .top){
-            Image(systemName: "person.circle")
-                .resizable()
-                .frame(width: 48, height: 48)
-            Text("こんにちは こんにちは こんにちは こんにちは こんにちは こんにちは こんにちは")
-                .padding()
-                .background(.white)
-                .cornerRadius(30)
-            VStack(alignment: .trailing){
-                Spacer()
-                Text("既読")
-                Text(timeStamp)
-            }
-            .foregroundColor(.secondary)
-            .font(.footnote)
+            userTumb
+            messageText
+            messageState
             Spacer()
             
         }
         .padding(.bottom)
     }
-    private var timeStamp: String{
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        return formatter.string(from: Date())
-    }
+    
 }
 
 #Preview {
     MessageRow()
         .background(.cyan)
+}
+
+
+extension MessageRow {
+    private var userTumb: some View{
+        Image(systemName: "person.circle")
+            .resizable()
+            .frame(width: 48, height: 48)
+    }
+    
+    private var messageText: some View{
+        Text("こんにちは こんにちは こんにちは こんにちは こんにちは こんにちは こんにちは")
+            .padding()
+            .background(.white)
+            .cornerRadius(30)
+    }
+    
+    private var messageState: some View{
+        VStack(alignment: .trailing){
+            Spacer()
+            Text("既読")
+            Text(timeStamp)
+        }
+        .foregroundColor(.secondary)
+        .font(.footnote)
+    }
+    
+    private var timeStamp: String{
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: Date())
+    }
 }
